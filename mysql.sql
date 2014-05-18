1292cc4ee038540dbdac0b13b29d06832375e92d
mrdanhdanh <nguyenhuudanh9999@gmail.com>
2014-05-18 13:38:35 -0700

    18/05

--- mysql.sql
+++ mysql.sql
@@ -1,1 +1,7 @@
-﻿ SELECT date,avg(value_ch4),avg(value_nm) FROM air_quality_data_5min WHERE to_number(to_char(time,'MI'),'99') BETWEEN 0 AND 15 GROUP BY date,to_char(time,'HH24')
+﻿ SELECT date,date_trunc('hour',time)+interval '15 minutes',avg(value_ch4),avg(value_nm)
+ FROM air_quality_data_5min
+ WHERE to_number(to_char(time,'MI'),'09')
+ BETWEEN 0 AND 14
+ GROUP BY date,date_trunc('hour',time)
+ ORDER BY date,date_trunc('hour',time);
+ 
